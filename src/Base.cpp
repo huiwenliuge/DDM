34d4f02214828910014df03d9f2f320e9b24a731
lzuhishop <1784517047@qq.com>
2016-12-31 17:37:26 +0800

    Update

--- src/Base.cpp
+++ src/Base.cpp
@@ -1,13 +1,49 @@
 #include "Base.hpp"
 
 float readInparm(const char* path){
-    FILE* fp = fopen(path,"r");
+    char* path_in=(char*)malloc(sizeof(char)*51);
+    sprintf(path_in,"%s%s\n",path,"in");
+    FILE* fp = fopen(path_in,"r");
     float ret;
     fscanf(fp,"#%f\n",&ret);
     return ret;
 }
 
 void prtOutparm(float out,const char* path){
-    FILE* fp = fopen(path,"w");
+    char* path_out=(char*)malloc(sizeof(char)*51);
+    sprintf(path_out,"%s%s\n",path,"out");
+    FILE* fp = fopen(path_out,"w");
     fprintf(fp,"#%.6f#",out);
+}
+
+void resetInparm(const char* path){
+    char* path_in=(char*)malloc(sizeof(char)*51);
+    sprintf(path_in,"%s%s\n",path,"in");
+    FILE* fp = fopen(path_in,"w");
+    fprintf(fp,"#%.6f#",(float)0.0);
+}
+
+Self readSelfparm(const char* path){
+    FILE* fp = fopen(path,"r");
+    float fac;
+    char* name = (char*)calloc(sizeof(char),51);
+    Self ret,tmp;
+    ret=NULL;
+    while(fscanf(fp,"%s: %f\n",name,&fac)!=-1){
+        tmp=(Self)malloc(sizeof(self));
+        tmp->factor=fac;
+        tmp->cellname=name;
+        tmp->next=ret;
+        ret=tmp;
+        name = (char*)calloc(sizeof(char),51);
+    }
+    return ret;
+}
+
+void prtSelfparm(Self tcell,const char* path){
+    FILE* fp = fopen(path,"w");
+    while(tcell){
+        fprintf(fp,"%s: %f\n",tcell->cellname,tcell->factor);
+        tcell=tcell->next;
+    }
 }
--- src/Base.hpp
+++ src/Base.hpp
@@ -5,7 +5,19 @@
 #include <fstream>
 #include <stdlib.h>
 
+struct self{
+    char* cellname;
+    float factor;
+    self* next;
+};
+
+typedef self* Self;
+
 float readInparm(const char* path);
 void prtOutparm(float var,const char* path);
+void resetInparm(const char* path);
+
+Self readSelfparm(const char* path);
+void prtSelfparm(Self tcell,const char* path);
 
 #endif
